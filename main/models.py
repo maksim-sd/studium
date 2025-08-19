6f0228d82d7662b2412c6ec9366ef5cd1b8c409a
Максим <s.makcum.d@gmail.com>
2025-08-19 12:42:15 +0900

    update models and creation modelsAdmin

--- main/models.py
+++ main/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Balance(models.Model):
     student = models.ForeignKey(User, on_delete=models.CASCADE, unique=True, verbose_name="Студент")
-    number_points = models.IntegerField(min=0, verbose_name="Количество баллов")
+    number_points = models.IntegerField(verbose_name="Количество баллов")
     
     class Meta:
         verbose_name = "Баланс"
@@ -26,7 +26,7 @@ class Product(models.Model):
     category_product = models.ForeignKey(CategoryProduct, on_delete=models.CASCADE, null=True, blank=True, verbose_name="Категория товара")
     name = models.CharField(max_length=40, verbose_name="Название")
     description = models.CharField(max_length=300, null=True, blank=True, verbose_name="Описание")
-    price = models.IntegerField(min=0, verbose_name="Цена")
+    price = models.IntegerField(verbose_name="Цена")
     photo = models.ImageField(null=True, blank=True, verbose_name="Фото")
     
     def __str__(self):
@@ -51,7 +51,7 @@ class PurchaseStatus(models.Model):
 class Purchase(models.Model):
     student = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Студент")
     product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name="Товар")
-    price = models.IntegerField(min=0, verbose_name="Цена")
+    price = models.IntegerField(verbose_name="Цена")
     purchase_status = models.ForeignKey(PurchaseStatus, on_delete=models.CASCADE, verbose_name="Статус покупки")
     
     class Meta:
@@ -75,7 +75,7 @@ class Chat(models.Model):
     datetime_creation = models.DateTimeField(auto_now=True, verbose_name="Дата и время создания")
     
     def __str__(self):
-        return self.datetime_creation + " " + self.chat_status.name
+        return self.id
     
     class Meta:
         verbose_name = "Чат"
@@ -127,18 +127,18 @@ class Tag(models.Model):
 
 
 class Task(models.Model):
-    customer = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Заказчик")
-    moderator = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Модератор")
-    executor = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Исполнитель")
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="customer_set", verbose_name="Заказчик")
+    moderator = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name="moderator_set", verbose_name="Модератор")
+    executor = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name="executor_set", verbose_name="Исполнитель")
     task_status = models.ForeignKey(TaskStatus, on_delete=models.CASCADE, verbose_name="Статус задачи")
     name = models.CharField(max_length=40, verbose_name="Название")
     description = models.CharField(max_length=300,verbose_name="Описание")
     type_reward = models.ForeignKey(TypeReward, on_delete=models.CASCADE, verbose_name="Тип вознаграждения")
-    amount_reward = models.IntegerField(min=0, verbose_name="Сумма вознаграждения", null=True, blank=True)
-    deadlines = models.ImageField(min=0, verbose_name="Сроки(количество дней)")
+    amount_reward = models.IntegerField(null=True, blank=True, verbose_name="Сумма вознаграждения")
+    deadlines = models.ImageField(verbose_name="Сроки(количество дней)")
     datetime_creation = models.DateTimeField(auto_now=True, verbose_name="Дата и время создания")
-    datetime_completion = models.DateTimeField(verbose_name="Дата и время завершения")
-    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, verbose_name="Чат")
+    datetime_completion = models.DateTimeField(null=True, blank=True, verbose_name="Дата и время завершения")
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, null=True, blank=True, verbose_name="Чат")
     
     def __str__(self):
         return self.name
@@ -159,7 +159,7 @@ class TaskTags(models.Model):
 
 class Feedback(models.Model):
     task = models.ForeignKey(Task, on_delete=models.CASCADE, verbose_name="Задача")
-    number_stars = models.IntegerField(min=0, max=5, verbose_name="Количество звезд")
+    number_stars = models.IntegerField(verbose_name="Количество звезд")
     comment = models.CharField(max_length=120, null=True, blank=True, verbose_name="Коментарий")
     
     class Meta:
